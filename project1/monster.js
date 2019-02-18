995422c6765f49224944557fde2b71e28fc99fac
Marissa Tracy <matr4187@colorado.edu>
2019-02-18 14:42:26 -0700

    fixed body selection, got rid of back button

--- project1/monster.js
+++ project1/monster.js
@@ -53,8 +53,10 @@ $("#lets_go_to_body").click(function() {
 	$("#choose_clothes").hide();
 	$("#start_over_page").hide();
 	$("#help_page").hide();
+	// Hide other body selection
 	$("#circle").hide();
-	$("#square").hide();
+	// Show "default" body
+	$("#square").show();
 });
 
 //goes to appearance tab when selected from navigation bar
@@ -65,33 +67,35 @@ $("#lets_go_to_body_nav").click(function() {
 	$("#choose_clothes").hide();
 	$("#start_over_page").hide();
 	$("#help_page").hide();
+	// Hide other body selection
 	$("#circle").hide();
+	// Show "default" body
 	$("#square").show();
 });
 
 //goes back to body tab when button is clicked
-$("#back_to_body").click(function() { 
-	$("#choose_body").show(); 
-	$("#welcome_page").hide(); 
-	$("#choose_name").hide();
-	$("#choose_clothes").hide();
-	$("#start_over_page").hide();
-	$("#help_page").hide();
-	$("#circle").hide();
-	$("#square").hide();
-});
+// $("#back_to_body").click(function() { 
+// 	$("#choose_body").show(); 
+// 	$("#welcome_page").hide(); 
+// 	$("#choose_name").hide();
+// 	$("#choose_clothes").hide();
+// 	$("#start_over_page").hide();
+// 	$("#help_page").hide();
+// 	$("#circle").hide();
+// 	$("#square").hide();
+// });
 
 //goes back to information tab when button is clicked
-$("#back_to_basic").click(function() { 
-	$("#choose_body").hide(); 
-	$("#welcome_page").hide(); 
-	$("#choose_name").show();
-	$("#choose_clothes").hide();
-	$("#start_over_page").hide();
-	$("#help_page").hide();
-	$("#circle").hide();
-	$("#square").hide();
-});
+// $("#back_to_basic").click(function() { 
+// 	$("#choose_body").hide(); 
+// 	$("#welcome_page").hide(); 
+// 	$("#choose_name").show();
+// 	$("#choose_clothes").hide();
+// 	$("#start_over_page").hide();
+// 	$("#help_page").hide();
+// 	$("#circle").hide();
+// 	$("#square").hide();
+// });
 
 //goes to clothes when "next" button is clicked
 $("#lets_go_to_clothes").click(function() { 
@@ -141,17 +145,7 @@ $("#done_button").click(function() {
 	$("#square").hide();
 });
 
-$("#choose_body_type").click(function() { 
-	$("#start_over_page").show(); 
-	$("#welcome_page").hide(); 
-	$("#choose_body").hide();
-	$("#choose_clothes").hide();
-	$("#choose_name").hide();
-	$("#help_page").hide();
-	$("#circle").hide();
-	$("#square").hide();
-});
-
+//displays help page when selected from navigation
 $("#help").click(function() { 
 	$("#start_over_page").hide(); 
 	$("#welcome_page").hide(); 
@@ -163,8 +157,10 @@ $("#help").click(function() {
 	$("#square").hide();
 });
 
+// Create JS function for each arrow
 $("#right_arrow").click(function() { 
 	$("#choose_body").show();
+	// Load either the square or cirlce body
 	$("#circle").show();
 	$("#welcome_page").hide(); 
 	$("#choose_name").hide();
@@ -176,7 +172,9 @@ $("#right_arrow").click(function() {
 
 $("#left_arrow").click(function() { 
 	$("#choose_body").show();
+	// Load the other body
 	$("#square").show();
+	// Hide other body
 	$("#circle").hide();
 	$("#welcome_page").hide(); 
 	$("#choose_name").hide();
@@ -184,17 +182,6 @@ $("#left_arrow").click(function() {
 	$("#start_over_page").hide();
 	$("#help_page").hide();
 });
-
-
-
-// $(document).ready(function(){
-// 	$("input[type='eye']").click(function(){
-// 	    var radioValue = $("input[name='eye']:checked").val();
-// 	    if(radioValue){
-// 	        alert("Your are a - " + radioValue);
-// 	    }
-// 	});
-// });
 
 
 
